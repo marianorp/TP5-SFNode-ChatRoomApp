d73eb4a7ff8369c28012c5078737a6fb65d115e2
RamiSammy <ramirolewin@hotmail.com>
2022-10-15 21:31:23 -0300

    Agregado historial json user y txt chat

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path')
 const formatMessage = require('./utils/messages')
 const {
@@ -28,7 +29,26 @@ io.on('connection', (socket) => {
   socket.on('joinRoom', ({ username, room }) => {
     const user = userJoin(socket.id, username, room)
 
+
+
     socket.join(user.room)
+
+    var nuevoHistorial = {
+      user : username , 
+      createdAT : new Date()
+    }
+
+    if(fs.existsSync("users.json")){
+      var userHistorial = JSON.parse(fs.readFileSync("users.json","utf-8"))
+      const registrado = userHistorial.find(index => index.user === username)
+      console.log(registrado)
+      if (registrado === undefined) {
+        userHistorial.push(nuevoHistorial)
+        fs.writeFileSync("users.json" , JSON.stringify(userHistorial))
+      }
+    }else {
+      fs.writeFileSync("users.json" , JSON.stringify([nuevoHistorial]))
+    } 
 
     //emit is used for only the user concerned
     socket.emit('message', formatMessage(chatName, 'Welcome to Chat Room'))
@@ -71,7 +91,17 @@ io.on('connection', (socket) => {
         io.to(user.room).emit(
           'message',
           formatMessage(user.username, msg)
-        )
+          )
+          
+          var nuevoMensaje = `[${new Date()}] ${username} : ${msg} (${user.room}) \n `
+
+        if(fs.existsSync("historialChat.txt")){
+          var historial = fs.readFileSync("historialChat.txt","utf-8")
+          historial += nuevoMensaje
+          fs.writeFileSync("historialChat.txt" , historial)
+        }else {
+          fs.writeFileSync("historialChat.txt" , nuevoMensaje)
+        } 
       
     })
   })
